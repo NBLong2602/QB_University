435007f93fa65bc36f0998f946f310b933452bef
NBLong2602 <balongforwork@gmail.com>
2023-02-27 20:12:51 +0700

    login page

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QB_University.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login() {
+            return View();
+        }
+    }
+}
