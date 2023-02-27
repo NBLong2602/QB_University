ef779506ed4a8354d9ab03b6d85328f6f894659b
NBLong2602 <balongforwork@gmail.com>
2023-02-27 17:56:00 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QB_University.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
